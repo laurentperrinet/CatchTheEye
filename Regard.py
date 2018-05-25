bca0d2a45331d34da2e9515e6e831f8c06bfaa6c
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-05-25 14:23:02 +0200

    refactooring Regard : Test set: Average loss: 0.0001, Accuracy: 459/459 (100%)

--- Regard.py
+++ Regard.py
@@ -10,6 +10,8 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torchvision.datasets import ImageFolder
 import torch.multiprocessing as mp
+import torchvision.models as models
+
 
 class Data():
     def __init__(self, args):
@@ -26,7 +28,7 @@ class Data():
 
         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
         t = transforms.Compose([
-            transforms.CenterCrop(150),
+            transforms.CenterCrop(256),
             transforms.ToTensor(),
             transforms.Normalize(mean=self.IMAGENET_MEAN, std=self.IMAGENET_STD),
             ])
@@ -51,28 +53,69 @@ class Data():
 
         return fig, ax
 
+
 class Net(nn.Module):
     def __init__(self, args):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.pool = nn.MaxPool2d(2, 2)#
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, args.dimension)
-        self.fc2 = nn.Linear(args.dimension, 10)
+        #self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(74420, args.dimension)
+        self.fc2 = nn.Linear(args.dimension, 4)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 4))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 4))
-        x = x.view(-1, 12800)
-        #x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
         #x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        #x = x.view(-1, 320)
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
+        #x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        x = F.dropout(x, training=self.training)
-        x = self.fc3(x)
         return F.log_softmax(x, dim=1)
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+# class Net(nn.Module):
+#     def __init__(self, args):
+#         super(Net, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+#         #self.pool = nn.MaxPool2d(2, 2)#
+#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+#         self.conv2_drop = nn.Dropout2d()
+#         self.fc1 = nn.Linear(320, args.dimension)
+#         self.fc2 = nn.Linear(args.dimension, 10)
+#
+#     def forward(self, x):
+#         x = F.relu(F.max_pool2d(self.conv1(x), 4))
+#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 4))
+#         x = x.view(-1, 20480)
+#         #x = F.relu(F.max_pool2d(self.conv1(x), 2))
+#         #x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+#         #x = x.view(-1, 320)
+#         x = F.relu(self.fc1(x))
+#         x = self.fc2(x)
+#         #x = F.dropout(x, training=self.training)
+#         x = self.fc3(x)
+#         return F.log_softmax(x, dim=1)
+
+# cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+# cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+#
+# # create a module to normalize input image so we can easily put it in a
+# # nn.Sequential
+# class Normalization(nn.Module):
+#     def __init__(self, mean, std):
+#         super(Normalization, self).__init__()
+#         # .view the mean and std to make them [C x 1 x 1] so that they can
+#         # directly work with image Tensor of shape [B x C x H x W].
+#         # B is batch size. C is number of channels. H is height and W is width.
+#         self.mean = torch.tensor(mean).view(-1, 1, 1)
+#         self.std = torch.tensor(std).view(-1, 1, 1)
 
 
 class ML():
@@ -91,10 +134,17 @@ class ML():
             torch.cuda.manual_seed(self.args.seed)
         # DATA
         self.d = Data(self.args)
-        self.model = Net(args).to(self.device)
         # MODEL
+        self.model = Net(args).to(self.device)
+        #self.model = models.vgg19(pretrained=True).features.to(device).eval()
         self.optimizer = optim.SGD(self.model.parameters(),
                                     lr=self.args.lr, momentum=self.args.momentum)
+
+
+    def forward(self, img):
+        # normalize img
+        return (img - self.mean) / self.std
+
 
     def train(self, rank=0):
         torch.manual_seed(self.args.seed + rank)
@@ -132,7 +182,7 @@ class ML():
 
         test_loss /= len(self.d.test_loader.dataset)
 
-        if args.log_interval>0:
+        if self.args.log_interval>0:
             print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
             test_loss, correct, len(self.d.test_loader.dataset),
             100. * correct / len(self.d.test_loader.dataset)))
