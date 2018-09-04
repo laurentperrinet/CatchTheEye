fb673ab0ef46084ac3c7ac9339dbd4ef7536e987
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-09-05 00:03:20 +0200

    notes

--- gaze.py
+++ gaze.py
@@ -160,10 +160,14 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), kernel_size=[self.args.stride1, self.args.stride1]))#, stride=[self.args.stride1, self.args.stride1]))
+            # s = self.bn1(self.conv1(s))        # batch_size x 32 x 64 x 64
         #x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), kernel_size=[2, 2], stride=[2, 2]))
         x = F.relu(F.max_pool2d(self.conv2(x), kernel_size=[self.args.stride2, self.args.stride2]))#, stride=[self.args.stride2, self.args.stride2]))
         x = x.view(-1, self.num_flat_features(x))
         x = F.relu(self.fc1(x))
+        #         # apply 2 fully connected layers with dropout
+        # s = F.dropout(F.relu(self.fcbn1(self.fc1(s))),
+        #     p=self.dropout_rate, training=self.training)    # batch_size x 128
         #x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
