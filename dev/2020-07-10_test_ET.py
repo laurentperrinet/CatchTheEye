7d0d6ac6561158f0a6c7769a6297798372741f4e
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2020-07-10 10:23:28 +0200

    Update 2020-07-10_test_ET.py

--- dev/2020-07-10_test_ET.py
+++ dev/2020-07-10_test_ET.py
@@ -4,15 +4,27 @@
 """ Using psychopy to perform an experiment on the role of a bias in the direction """
 
 import os
-from LeCheapEyeTracker.EyeTrackerServer import Server
+
 import imageio
 
-et = Server()
-camera = et.cam
+if False:
+    from LeCheapEyeTracker.EyeTrackerServer import Server
+    et = Server()
+    camera = et.cam
+else:
+    import cv2
+    camera = cv2.VideoCapture(0)
+    def grab():
+        ret, frame_bgr = camera.read()
+        return ret, frame_bgr
+
 import time
 
 for i in range(5):
-    print('i=', i)
-    frame = camera.grab()
+    print('i=', i+1)
+    frame = False
+    while not (frame):
+        ret, frame = camera.grab()
+        print(ret, frame)
     imageio.imwrite(f'/tmp/{i}.png', frame[:, :, ::-1]) # converting on the fly from BGR > RGB
     time.sleep(1)
